d3a3f7e026c90622e46d1afea0e1c37051aca8b1
Margrét María Hólmarsdóttir <margretmariaholmarsdottir@clients-pool-6.nat.ru.is>
2018-09-04 10:57:18 +0000

    finish up max positive

--- maxpositive.py
+++ maxpositive.py
@@ -1,4 +1,13 @@
+max_int = 0
 
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+while True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+
+    if num_int > 0:
+        if num_int > max_int:
+            max_int = num_int
+                
+    else:
+        break
+    # Fill in the missing code
 print("The maximum is", max_int)    # Do not change this line
