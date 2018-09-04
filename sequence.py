2dfbf0e7904f02386ca461f8ee4c5d2ef8431da8
Margrét María Hólmarsdóttir <margretmariaholmarsdottir@clients-pool-6.nat.ru.is>
2018-09-04 11:35:14 +0000

    sett inn gildi fyrir 3 tölur

--- sequence.py
+++ sequence.py
@@ -1,4 +1,24 @@
 # Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 
+# ætla fyrst að setja inn input
+
+# ætla svo að setja output
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+tala1 = 1
+tala2 = 2
+tala3 = 3
+sum = 0
+
+for i in range (1, n +1):
+    if i == 1 or i == 2 or i == 3:
+        print (i)
+    else:  
+        sum = tala1 + tala2 + tala3
+        print(sum)
+        tala1 = tala2
+        tala2 = tala3
+        tala3 = sum 
+
+
